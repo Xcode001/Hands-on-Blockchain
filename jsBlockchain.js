c71585b47e66c1729a7edbb2a6c96a8e905676ac
Musa Musa <musamusa739@gmail.com>
2018-11-03 13:17:38 +0100

    Implementing Security hash validation of the blockchain

--- jsBlockchain.js
+++ jsBlockchain.js
@@ -41,6 +41,24 @@ class BlockChain{
         newBlock.hash = newBlock.calculateHash(); //hash of current block
         this.chain.push(newBlock); //push the block the the chain of blocks
     }
+
+// function definition for hash validation for the block
+    checkBlockChainValid(){
+        for(let i=1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let block1 = new Block(1, "12/3/2018",{mybalance: 100});
@@ -51,5 +69,7 @@ myBlockChain.addBlock(block1);
 myBlockChain.addBlock(block2);
 
 console.log(JSON.stringify(myBlockChain,null,4));
+console.log("Validation check for the Block Chain: " +myBlockChain.checkBlockChainValid());
+
 
 
